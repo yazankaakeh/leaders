3bcbf2e5be36349642ad900228b20c811a5348d1
Yazan Kaakeh <yazanka187@gmail.com>
2022-09-29 23:18:02 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,11 +49,14 @@
                 <h1>
                     I'm Yazan Kaakeh
                 </h1>
+
+                <h1>
                     I am Nour
                 </h1>
+
                 <h1>
                     Im Ahmed Alkatib
-                <h1>
+                </h1>
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
